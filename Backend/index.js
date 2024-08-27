2d1609ed6d68ec87d191fc8dca865cc6b9344034
Gaurav2418 <123626617+Gaurav2418@users.noreply.github.com>
2024-08-27 22:12:04 +0530

    test 2

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,15 @@
-<h1>Hello World</h1>
+const express = require( 'express' );
+
+const app = express();
+const port = 3000;
+
+
+// app.get('/', (request, response) => {
+//     console.log(request);
+//     return response.status(234).send('Welcome To MERN Stack Tutorial');
+//   });
+
+
+app.listen(port, ()=>{
+    console.log(`app is listining on ${port}` )
+});
