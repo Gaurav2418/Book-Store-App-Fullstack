bcec97044688c45003bd762ce2d6bab585454ef4
Gaurav2418 <123626617+Gaurav2418@users.noreply.github.com>
2024-08-29 10:58:32 +0530

    started with frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='bg-blue-300 text-white font-bold'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
